263773dddaf42bcf940e7bf7eb76aa9f549046d6
MIDANI <32643487+MIDANI-Bassem@users.noreply.github.com>
2024-03-10 12:14:46 +0100

    adding layout and index.html

--- airline/flights/views.py
+++ airline/flights/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
+from .models import Flight
 
 # Create your views here.
 def index():
-    return
+    return render(request, "flights/index.html",{
+        "flights" : Flight.objects.all()
+    })
